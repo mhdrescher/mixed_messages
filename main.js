46d07a6ace1e6312ce93095acd3fa53889835fbf
mhd1 <98111598+mhdrescher@users.noreply.github.com>
2022-01-21 09:04:30 +0100

    Empty Objects created

---
+++ main.js
@@ -0,0 +1,10 @@
+// Creating Objects for random messages 
+
+const randomMessages1 = {};
+
+const randomMessages2 = {};
+
+const randomMessages3 = {};
+
+// Setting up factory function to fill in further messages to above objects
+
