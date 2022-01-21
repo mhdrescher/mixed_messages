07772516d25179c8e76ee3907c7f12b8d30ebabf
mhd1 <98111598+mhdrescher@users.noreply.github.com>
2022-01-21 17:44:13 +0100

    test branch

--- main.js
+++ main.js
@@ -1,8 +1,38 @@
 // Creating Objects for random messages
 // Can be filled with addMessage functions, see file. 
 
-const randomMessages1 = [];
+const randomMessages1 = [
+    {message: 'Message 1'},
+    {message: 'Message 2'},
+    {message: 'Message 3'},
+    {message: 'Message 4'},
+    {message: 'Message 5'},
+    {message: 'Message 6'},
+    {message: 'Message 7'},
+    {message: 'Message 8'},
+    {message: 'Message 9'}
+];
 
-const randomMessages2 = [];
+const randomMessages2 = [
+    {message: 'Message A'},
+    {message: 'Message B'},
+    {message: 'Message C'},
+    {message: 'Message D'},
+    {message: 'Message E'},
+    {message: 'Message F'},
+    {message: 'Message G'},
+    {message: 'Message H'},
+    {message: 'Message I'}
+];
 
-const randomMessages3 = [];
+const randomMessages3 = [
+    {message: 'Message a'},
+    {message: 'Message b'},
+    {message: 'Message c'},
+    {message: 'Message d'},
+    {message: 'Message e'},
+    {message: 'Message f'},
+    {message: 'Message g'},
+    {message: 'Message h'},
+    {message: 'Message i'}
+]
