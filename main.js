e89bfd008b733c4ff2b19954f5a80eb5a18e3b02
mhd1 <98111598+mhdrescher@users.noreply.github.com>
2022-01-23 20:14:20 +0100

    Weird file

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // Creating Objects for random messages
 // Can be filled with addMessage functions, see file. 
 
+const randomMessages1Starter = 'Starter 1: '
 const randomMessages1 = [
     {message: 'Message 1'},
     {message: 'Message 2'},
@@ -13,6 +14,7 @@ const randomMessages1 = [
     {message: 'Message 9'}
 ];
 
+const randomMessages2Starter = 'Starter 2: '
 const randomMessages2 = [
     {message: 'Message A'},
     {message: 'Message B'},
@@ -25,6 +27,7 @@ const randomMessages2 = [
     {message: 'Message I'}
 ];
 
+const randomMessages3Starter = 'Starter 3: '
 const randomMessages3 = [
     {message: 'Message a'},
     {message: 'Message b'},
@@ -36,3 +39,8 @@ const randomMessages3 = [
     {message: 'Message h'},
     {message: 'Message i'}
 ]
+
+console.log(randomMessages1Starter + randomMessages1[Math.floor(Math.random() * randomMessages1.length)].message)
+console.log(randomMessages2Starter + randomMessages2[Math.floor(Math.random() * randomMessages2.length)].message)
+console.log(randomMessages3Starter + randomMessages3[Math.floor(Math.random() * randomMessages3.length)].message)
+
